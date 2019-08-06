e1a20a56693192c5aa8857dd44a7ba46eed0349c
Yehia Hassan <yehia4747@gmail.com>
2019-08-06 21:44:25 +0200

    add speller

---
+++ pset4/dictionary.c
@@ -0,0 +1,121 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Represents number of buckets in a hash table
+#define N 26
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Represents a hash table
+node *hashtable[N];
+
+unsigned long words = 0;
+
+// Hashes word to a number between 0 and 25, inclusive, based on its first letter
+unsigned int hash(const char *word)
+{
+    return tolower(word[0]) - 'a';
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    // Initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        hashtable[i] = NULL;
+    }
+
+    // Open dictionary
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        unload();
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+    // Insert words into hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // TODO
+        // Hash the word
+        int index = hash(word);
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            unload();
+            return false;
+        }
+        strcpy(new_node->word, word);
+        new_node->next = hashtable[index];
+        hashtable[index] = new_node;
+        words++;
+    }
+
+    // Close dictionary
+    fclose(file);
+
+    // Indicate success
+    return true;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return words;
+}
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    // TODO
+    int index = hash(word);
+    node *head = hashtable[index];
+    node *cursor = head;
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        node *head = hashtable[i];
+        node *cursor = head;
+
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
+}
